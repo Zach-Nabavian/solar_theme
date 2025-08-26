be997d23db44656fded4433aa05e8560c07ce425
Zach-Nabavian <107444420+Zach-Nabavian@users.noreply.github.com>
2025-08-25 21:03:39 -0400

    Update Solar Theme.R

--- Solar Theme.R
+++ Solar Theme.R
@@ -6,11 +6,11 @@ library(ggplot2)
 theme_solar <- function() {
   ggplot2::theme_minimal() +
     ggplot2::theme(
-      text = ggplot2::element_text(family = "Arial", size = 15),
+      text = ggplot2::element_text(family = "Arial", size = 11),
       plot.title = ggplot2::element_text(size = 24, face = "bold", color = "white"),
       plot.subtitle    = element_text(size = 20, color = "white", margin = margin(b = 10)),
-      axis.text = ggplot2::element_text(size = 15, color = "white"),
-      axis.title = ggplot2::element_text(size = 18, color = "white"),
+      axis.text = ggplot2::element_text(size = 12, color = "white"),
+      axis.title = ggplot2::element_text(size = 15, color = "white"),
       panel.grid.major = ggplot2::element_line(color = "gray80"),
       panel.grid.minor = ggplot2::element_line(color = "gray90"),
       panel.background = ggplot2::element_rect(fill = "black", color = NA),
